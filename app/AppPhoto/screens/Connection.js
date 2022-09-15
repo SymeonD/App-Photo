06ca0075b1056d78359a913bc73a730e75c68dd0
SymeonD <symeon.dealmeida@gmail.com>
2022-09-15 15:22:10 +0200

    Svg starter

--- app/AppPhoto/screens/Connection.js
+++ app/AppPhoto/screens/Connection.js
@@ -6,6 +6,9 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 //import { TextInput } from 'react-native-paper';
 //import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
+import { useSpring, animated, config } from "@react-spring/native";
+import svg from 'react-native-svg';
+
 function ConnectionScreen({navigation}) {
     const [pseudoConnect, setPseudoConnect] = useState('');
     const [passwordConnect, setPasswordConnect] = useState('');
@@ -38,6 +41,16 @@ function ConnectionScreen({navigation}) {
                 }
             })
     })
+
+    //SVG Blob
+    const options = {
+        complexity: 0.3,
+        contrast: 0.6,
+        guides: true,
+        size: 1000,
+        color: "hsl(6.13, 73.23%, 49.8%)",
+        stroke: 0
+      };
 
     function selectImage() {
         let options = {
@@ -266,6 +279,23 @@ function ConnectionScreen({navigation}) {
                 style={styles.image} 
                 source={require("../assets/log2.png")}
             />
+
+            <svg viewBox="0 0 600 600" className="wheel" width="600">
+                <defs>
+                    <radialGradient id="radial-gradient" cx="10%" fx="10%" fr="0%" r="80%">
+                    <stop offset="20%" stop-color="#5568C3" />
+                    <stop offset="95%" stop-color="#27336F" />
+                    </radialGradient>
+                </defs>
+
+                {
+                    // <Lines />
+                    
+                }
+                <g transform="translate(0,0)">
+                    <animated.path class="blob-gradient" d={svg} />
+                </g>
+            </svg>
             
             {isUserExisting != false ? 
             <Text style={{color:'#ed2f2f', marginBottom: 5}}>
